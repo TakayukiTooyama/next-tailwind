efdb7cf163dcbd17e434faa813848c86aae63570
TakayukiTooyama <t.tooyama107@gmail.com>
2023-02-28 17:21:14 +0900

    :recycle: Next.js13.2対応

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,17 @@
 import '@/style/global.css';
 
-import type { NextPage } from 'next';
+import type { Metadata, NextPage } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    default: 'Next x Tailwind Sterter',
+    template: '%s | Next x Tailwind Sterter',
+  },
+  description: 'Next.js13 x TailwindCSS テンプレート',
+  icons: {
+    icon: '/favicon.ico',
+  },
+};
 
 type RootLayoutProps = {
   children: React.ReactNode;
